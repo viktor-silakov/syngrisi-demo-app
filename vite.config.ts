cdc58aec70ecf844c9da2d7a24d3a907e5e69f07
Viktar <1105714@gmail.com>
2023-11-04 14:36:11 +0400

    fix vite base path

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    base: './',
+    // root: '/syngrisi-demo-app/',
+    base: '/syngrisi-demo-app/',
     plugins: [react()],
 });
